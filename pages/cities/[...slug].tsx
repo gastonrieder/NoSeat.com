35d1426a36b5f05fe2208bdb9f8df6532b9d7814
gaston-rieder <gastonrieder@gmail.com>
2024-11-28 21:52:50 +0100

    some formatting edits

--- pages/cities/[...slug].tsx
+++ pages/cities/[...slug].tsx
@@ -57,6 +57,7 @@ export default function CityDoc({ frontmatter, code, headerImage }: Doc) {
             </Flex>
   );
 }
+
 export async function getStaticProps(
   context: GetStaticPropsContext<{ slug: string[] }>,
 ) {
