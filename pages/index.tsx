79fc10453003bac51ce6f4fe82c3da6056bb1533
gaston-rieder <gastonrieder@gmail.com>
2024-12-05 10:54:36 +0100

    added search bar directly on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { getMDXComponent } from "mdx-bundler/client";
-import { Box, Flex } from "@radix-ui/themes";
+import { Box, Flex, Heading, Text} from "@radix-ui/themes";
 import { MDXProvider } from "@components/MDXComponents";
 import { ThemesMDXComponents } from "@components/ThemesMDXComponents";
 import { getMdxBySlug } from "@utils/mdx";
 import { getTransportImage } from "@utils/unsplash";
 import { ThemesDocsPage } from "@components/ThemesDocsPage";
 import { Footer } from "@components/Footer";
+import { PrimitivesSearchDesktop } from "@components/PrimitivesSearchDesktop";
+import { DocsNav } from "@components/DocsNav";
+import { themesRoutes } from "@utils/themesRoutes";
 
 type HomeProps = {
   frontmatter: {
@@ -23,31 +26,52 @@ export default function Home({ frontmatter, code, headerImage }: HomeProps) {
 
   return (
     <Flex direction="column" style={{ minHeight: '100vh' }}>
-    <ThemesDocsPage>
-      <Box mb="3">
-        <img 
-          src={headerImage} 
-          alt={`Public Transport Guide`}
-          style={{
-            width: '100%',
-            height: '250px',
-            objectFit: 'cover',
-            borderRadius: 'var(--radius-3)'
-          }}
-        />
-      </Box>
+      <ThemesDocsPage>
+        <Box mb="3">
+          <img 
+            src={headerImage} 
+            alt={`Public Transport Guide`}
+            style={{
+              width: '100%',
+              height: '250px',
+              objectFit: 'cover',
+              borderRadius: 'var(--radius-3)'
+            }}
+          />
+        </Box>
 
-      <MDXProvider frontmatter={frontmatter}>
-        <Component components={ThemesMDXComponents as any} />
-      </MDXProvider>
-    </ThemesDocsPage>
-          <Flex justify="center" mt="auto">
-          <Footer />
-        </Flex>
+        <Heading as="h1" size="8" mb="2">
+          {frontmatter.metaTitle}
+        </Heading>
+        <Text size="3" mb="4">
+          {frontmatter.metaDescription}
+        </Text>
+
+        <Box 
+          display={{ initial: 'block', md: 'none' }} 
+          mb="4" 
+          style={{ 
+            borderTop: '1px solid var(--gray-6)',
+            paddingTop: '16px',
+            marginTop: '16px'
+          }}
+        >
+          <PrimitivesSearchDesktop />
+          <Box mt="4">
+            <DocsNav routes={themesRoutes} />
+          </Box>
+        </Box>
+        
+        <MDXProvider frontmatter={frontmatter}>
+          <Component components={ThemesMDXComponents as any} />
+        </MDXProvider>
+      </ThemesDocsPage>
+      <Flex justify="center" mt="auto">
+        <Footer />
       </Flex>
+    </Flex>
   );
-}
-export async function getStaticProps() {
+}export async function getStaticProps() {
   const { frontmatter, code } = await getMdxBySlug("templates/", "home");
   const headerImage = await getTransportImage("public transport");
 
